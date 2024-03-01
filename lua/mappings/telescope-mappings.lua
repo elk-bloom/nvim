e3b99e167ace3f46ce5ba376d0505c6b5f420622
elk-bloom <152300047+elk-bloom@users.noreply.github.com>
2024-03-01 15:28:05 -0500

    change

--- lua/mappings/telescope-mappings.lua
+++ lua/mappings/telescope-mappings.lua
@@ -6,14 +6,15 @@ function M.setup()
         ["<leader>"] = {
             f = {
                 name = "Find",
-                f = { "<cmd>Telescope find_files<cr>", "Find Files" },
-                F = { "<cmd>Telescope find_files cwd=false<cr>", "Find Files (no cwd)" },
+                f = { "<cmd>Telescope git_files<cr>", "Find Git Files" },
+                F = { "<cmd>Telescope git_files cwd=false<cr>", "Find Git Files (no cwd)" },
+                v = { "<cmd>Telescope find_files<cr>", "Find Files" },
+                V = { "<cmd>Telescope find_files cwd=false<cr>", "Find Files (no cwd)" },
                 b = { "<cmd>Telescope buffers<cr>", "Buffers" },
                 h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
                 r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
                 p = { "<cmd>Telescope projects<cr>", "Projects" },
             },
-            -- I would prefer the grep ones to be under `s` for search, so make those edits
             s = {
                 g = { "<cmd>Telescope live_grep<cr>", "Grep (root dir)" },
                 G = { "<cmd>Telescope live_grep cwd=false<cr>", "Grep (cwd)" },
